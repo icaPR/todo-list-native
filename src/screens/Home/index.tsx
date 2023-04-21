f5f038c397d2d61a98db8ad968d7e0a58bf1b6b1
Icaro Ramos <polosnramos@hotmail.com>
2023-04-21 20:10:29 -0300

    feat: Tasks info

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,13 @@
 import { Text, TextInput, View, Image } from "react-native";
 import { styles } from "./styles";
 import { TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Icon from "react-native-vector-icons/Ionicons";
 
 export default function Home() {
+  const [countCreate, setcountCreate] = useState<Number>(0);
+  const [countComplete, setcountComplete] = useState<Number>(0);
+
   return (
     <View style={styles.container}>
       <View style={styles.upContainer}>
@@ -20,6 +23,16 @@ export default function Home() {
           <TouchableOpacity style={styles.button}>
             <Icon name="add-circle-outline" color="#ffffff" size={20} />
           </TouchableOpacity>
+        </View>
+        <View style={styles.info}>
+          <View style={styles.infoCreate}>
+            <Text style={styles.textCreate}>Criadas</Text>
+            <Text style={styles.numberInfo}>{countCreate}</Text>
+          </View>
+          <View style={styles.infoComplete}>
+            <Text style={styles.textComplete}>Concluídas</Text>
+            <Text style={styles.numberInfo}>{countComplete}</Text>
+          </View>
         </View>
       </View>
     </View>
